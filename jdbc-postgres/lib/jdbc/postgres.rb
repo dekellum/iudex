450994d3ee4296a767757bfa11fe6eb59625a229
David Kellum <david@gravitext.com>
2009-01-04 13:16:33 -0800

    * Merge jdbc-postgres (updated postgreql driver).

---
+++ jdbc-postgres/lib/jdbc/postgres.rb
@@ -0,0 +1,10 @@
+module Jdbc
+  module Postgres
+    VERSION = "8.3" 
+  end
+end
+if RUBY_PLATFORM =~ /java/
+  require "postgresql-#{Jdbc::Postgres::VERSION}-603.jdbc4.jar"
+else
+  warn "jdbc-postgres is only for use with JRuby"
+end
