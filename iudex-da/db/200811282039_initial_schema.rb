0aafb5bbe5c1c0ab1e03ad51f07a1e4c0794bb2b
David Kellum <dek-oss@gravitext.com>
2009-03-14 13:09:37 -0700

    Simplify/fix migrations.

--- iudex-da/db/200811282039_initial_schema.rb
+++ iudex-da/db/200811282039_initial_schema.rb
@@ -1,4 +1,20 @@
-# http://api.rubyonrails.org/classes/ActiveRecord/Migration.html
+#--
+# Copyright (C) 2008-2009 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+# See http://api.rubyonrails.org/classes/ActiveRecord/Migration.html
 
 class InitialSchema < ActiveRecord::Migration
   def self.up
@@ -10,7 +26,7 @@ class InitialSchema < ActiveRecord::Migration
       t.datetime 'last_visit'
     end
     execute "ALTER TABLE urls ADD PRIMARY KEY (uhash)"
-    add_index 'urls', [ 'host' ], :name => 'index_urls_on_host'
+    add_index 'urls', [ 'host' ]
   end
 
   def self.down
--- iudex-da/db/200811282054_add_visit_after.rb
+++ iudex-da/db/200811282054_add_visit_after.rb
@@ -1,14 +1,27 @@
-# http://api.rubyonrails.org/classes/ActiveRecord/Migration.html
+#--
+# Copyright (C) 2008-2009 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
 
 class AddVisitAfter < ActiveRecord::Migration
   def self.up
     add_column 'urls', 'next_visit_after', :datetime
-    add_index( 'urls', [ 'next_visit_after' ], 
-               :name => 'index_urls_on_next_visit_after' )
+    add_index( 'urls', [ 'next_visit_after' ] )
   end
 
   def self.down
-    remove_index 'urls', 'index_urls_on_next_visit_after'
+    remove_index  'urls', 'next_visit_after'
     remove_column 'urls', 'next_visit_after'
   end
 end
