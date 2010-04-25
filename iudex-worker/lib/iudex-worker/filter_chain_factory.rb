be9eefa6adc74b4c67bfbc36ad98783af2222c4f
David Kellum <dek-oss@gravitext.com>
2010-04-24 17:46:43 -0700

    worker: filter_chain_factory dervied from DA

--- iudex-worker/lib/iudex-worker/filter_chain_factory.rb
+++ iudex-worker/lib/iudex-worker/filter_chain_factory.rb
@@ -14,10 +14,8 @@
 # permissions and limitations under the License.
 #++
 
-require 'iudex-core'
-require 'iudex-core/filter_chain_factory'
-
 require 'iudex-da'
+require 'iudex-da/filter_chain_factory'
 require 'iudex-da/pool_data_source_factory'
 
 require 'iudex-httpclient-3' #FIXME: More central?
@@ -26,57 +24,25 @@ require 'iudex-rome'
 module Iudex
   module Worker
 
-    class FilterChainFactory < Iudex::Core::FilterChainFactory
-      import 'iudex.core.filters.UHashMDCSetter'
-      import 'iudex.core.filters.ContentFetcher'
-      import 'iudex.core.filters.TextCtrlWSFilter'
+    class FilterChainFactory < Iudex::DA::FilterChainFactory
 
-      import 'iudex.da.feed.FutureDateFilter'
-      import 'iudex.da.feed.FeedWriter'
+      import 'iudex.httpclient3.HTTPClient3'
+      import 'iudex.rome.RomeFeedParser'
 
-      import 'iudex.httpclient3.HTTPClient3' #FIXME
-      import 'iudex.rome.RomeFeedParser' #FIXME
-
-      def feed_post
-        [ RomeFeedParser.new, #FIXME: dep leak.
-          feed_writer ]
-      end
-
-      def feed_writer
-        f = FeedWriter.new( data_source, [] )
-
-        create_chain( "feed-update", feed_update ) { |c| f.update_ref_filter = c }
-        create_chain( "feed-new",    feed_new )    { |c| f.new_ref_filter = c }
-        f
-      end
-
-      # FIXME: Central configuration?
-      def http_client
+      # FIXME: More Central configuration?
+      def initialize( name )
+        super( name )
         @http_mf = RJack::HTTPClient3::ManagerFacade.new
         @http_mf.start
-        HTTPClient3.new( @http_mf.client )
+        self.http_client = HTTPClient3.new( @http_mf.client )
+
         #FIXME: Close with FilterChainFactory?
+
+        self.data_source = Iudex::DA::PoolDataSourceFactory.new.create
       end
 
-      def feed_filters
-        [ ContentFetcher.new( http_client,
-                              create_chain( "feed-receiver", feed_post ) ) ]
-      end
-
-      def feed_update
-        [ TextCtrlWSFilter.new( ContentKeys::TITLE ),
-          FutureDateFilter.new( ContentKeys::PUB_DATE ) ]
-      end
-
-      def feed_new
-        [ TextCtrlWSFilter.new( ContentKeys::TITLE ),
-          FutureDateFilter.new( ContentKeys::PUB_DATE ) ]
-      end
-
-      # FIXME: From DA?
-      def data_source
-        factory = Iudex::DA::PoolDataSourceFactory.new
-        factory.create
+      def feed_post
+        [ RomeFeedParser.new ] + super
       end
 
     end
