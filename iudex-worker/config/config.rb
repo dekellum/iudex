0a22e9dd87817bf4e3c1729bccecec6c399fc4ea
David Kellum <dek-oss@gravitext.com>
2010-05-27 11:02:03 -0700

    worker: Add http sample config, require's for testing.

--- iudex-worker/config/config.rb
+++ iudex-worker/config/config.rb
@@ -1,4 +1,7 @@
 
+require 'iudex-da'
+require 'iudex-httpclient-3'
+
 Iudex.configure do |c|
 
   # Set DA connection properties
@@ -9,5 +12,9 @@ Iudex.configure do |c|
     :ds_pool     => { :max_active => 4,
                       :max_idle   => 2 },
     :loglevel => 2 }
+
+  c.setup_http_client_3 do |mgr|
+    mgr.manager_params.max_total_connections = 200
+  end
 
 end
