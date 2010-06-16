4c667604e8925a8f90281332b0ac0ba87a1e0483
David Kellum <dek-oss@gravitext.com>
2010-06-15 20:07:33 -0700

    core: Headers/javadoc

--- iudex-core/src/main/java/iudex/core/WorkPollStrategy.java
+++ iudex-core/src/main/java/iudex/core/WorkPollStrategy.java
@@ -1,8 +1,32 @@
+/*
+ * Copyright (c) 2008-2010 David Kellum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package iudex.core;
 
 public interface WorkPollStrategy
 {
+    /**
+     * Returns true to indicate that the current visitQ should be shutdown
+     * and a new VisitQueue created.
+     */
     boolean shouldReplaceQueue( VisitQueue visitQ );
 
+    /**
+     * @param current VisitQueue or null, requiring new queue.
+     * @return existing or new VisitQueue
+     */
     VisitQueue pollWork( VisitQueue current );
 }
