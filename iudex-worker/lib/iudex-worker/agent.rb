90c20f569c0420606c62def69081718e94fec098
David Kellum <dek-oss@gravitext.com>
2010-06-26 18:26:48 -0700

    worker: Initial standalone agent and launcher

---
+++ iudex-worker/lib/iudex-worker/agent.rb
@@ -0,0 +1,89 @@
+#--
+# Copyright (c) 2008-2010 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+require 'iudex-da'
+require 'iudex-da/pool_data_source_factory'
+
+require 'iudex-httpclient-3'
+
+require 'iudex-worker'
+require 'iudex-worker/filter_chain_factory'
+
+module Iudex
+  module Worker
+
+    class Agent
+      include Iudex::DA
+      include Iudex::Core
+      include Iudex::HTTPClient3
+      include Iudex::Worker
+      include Gravitext::HTMap
+
+      def initialize
+        Config.do_agent( self )
+      end
+
+      def poll_keys
+        [ :uhash,
+          :host,
+          :url,
+          :type,
+          :ref_pub_date,
+          :priority,
+          :next_visit_after ]
+      end
+
+      def run
+        dsf = PoolDataSourceFactory.new
+        data_source = dsf.create
+
+        cmapper = ContentMapper.new( kmap( poll_keys ) )
+        wpoller = WorkPoller.new( data_source, cmapper )
+
+        mgr = Config.do_http_client_3
+        mgr.start
+
+        fcf = FilterChainFactory.new( 'agent' )
+        fcf.http_client = Iudex::HTTPClient3::HTTPClient3.new( mgr.client )
+        fcf.data_source = data_source
+
+        Config.do_filter_factory( fcf )
+
+        fcf.filter do |chain|
+          vexec = VisitExecutor.new( chain, wpoller )
+          Config.do_visit_executor( vexec )
+          vexec.start
+          vexec.join    #Run until interrupted
+        end # fcf closes
+
+        mgr.shutdown
+        dsf.close
+
+      end
+
+      #FIXME: Push down to filter or gravitext.util?
+      def kmap( keys )
+        keys.map do |ks|
+          ContentMapper::LOGICAL_KEYS.get( ks.to_s ) or
+            UniMap::KEY_SPACE.get( ks.to_s ) or
+            raise( "Key #{ks} not found" )
+        end
+      end
+
+    end
+
+  end
+end
