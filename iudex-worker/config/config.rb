2eb31096f06477f0dd3027b13b3a338ae2734ac8
David Kellum <dek-oss@gravitext.com>
2010-07-01 21:11:53 -0700

    worker: Config sample, worker-fg with module includes

--- iudex-worker/config/config.rb
+++ iudex-worker/config/config.rb
@@ -1,42 +1,42 @@
 
-require 'iudex-da'
-require 'iudex-httpclient-3'
+module LocalIudexConfig
+  include RJack
 
-module RJack::Logback
-  logger( 'iudex.filter.core.FilterChain.agent' ).level = DEBUG
-end
+  Logback[ 'iudex.filter.core.FilterChain.agent' ].level = Logback::DEBUG
 
-Iudex.configure do |c|
+  Iudex.configure do |c|
 
-  concurrency = 3
+    threads = 3
 
-  c.connect_props = {
-    :ds_pool  => { :max_active => concurrency,
-                   :max_idle   => concurrency * 2 / 3 },
-    :loglevel => 1
-  }
+    c.connect_props = {
+      :ds_pool  => { :max_active => threads / 3 * 2,
+                     :max_idle   => threads / 3 },
+      :loglevel => 1
+    }
 
-  c.setup_http_client_3 do |mgr|
-    mgr.manager_params.max_total_connections = 100
-  end
-
-  c.setup_visit_executor do |vx|
-    vx.max_threads = concurrency
-    vx.min_host_delay = 20
-  end
-
-  c.setup_filter_factory do |ff|
-
-    def ff.barc_writer
-      bw = super
-      bw.do_compress = false
-      bw
+    c.setup_http_client_3 do |mgr|
+      mgr.manager_params.max_total_connections = threads * 10
     end
 
-    def ff.barc_directory
-      bdir = super
-      bdir.setTargetBARCLength( 64 * 1024 )
-      bdir
+    c.setup_visit_executor do |vx|
+      vx.max_threads = threads
+      vx.min_host_delay = 1 #ms : none!
+    end
+
+    c.setup_filter_factory do |ff|
+
+      def ff.barc_writer
+        bw = super
+        bw.do_compress = false
+        bw
+      end
+
+      def ff.barc_directory
+        bdir = super
+        bdir.target_length = 64 * 1024
+        bdir
+      end
+
     end
 
   end
