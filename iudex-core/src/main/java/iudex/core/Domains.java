edc3e77dc1b100fbaca91a9d11fe89d38a4a55c3
David Kellum <dek-oss@gravitext.com>
2010-10-23 16:55:51 -0700

    core: Add RLDomainFilter to imports; cosmetic

--- iudex-core/src/main/java/iudex/core/Domains.java
+++ iudex-core/src/main/java/iudex/core/Domains.java
@@ -34,11 +34,11 @@ public final class Domains
             final String parent = parent( name );
 
             if( TLDSets.TLD_PARENTS.contains( parent ) ) {
-               return TLDSets.REG_EXCEPTIONS.contains( name  ) ? name : child;
-           }
+                return TLDSets.REG_EXCEPTIONS.contains( name  ) ? name : child;
+            }
 
-           child = name;
-           name = parent;
+            child = name;
+            name = parent;
         }
         return null;
     }
