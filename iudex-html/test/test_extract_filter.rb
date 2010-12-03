8dc3b6ec8bfa8b8aee8ed0aa8ac34302d941b9be
David Kellum <dek-oss@gravitext.com>
2010-12-03 07:06:33 -0800

    html: Improve TestExtractFilter

--- iudex-html/test/test_extract_filter.rb
+++ iudex-html/test/test_extract_filter.rb
@@ -28,21 +28,75 @@ class TestExtractFilter < MiniTest::Unit::TestCase
   Order = HTMLTreeFilter::Order
 
   def test_doc
-    html = <<HTML
-<div>
- <p>Short junk</p>
- <hr/>
- <p>A more <i>substantive </i>paragraph.</p>
- <p>A similarly <i>substantive </i>paragraph.</p>
-</div>
-HTML
-    map = content( html )
-    fc = [ HTMLTreeFilter.new( :source_tree.to_k,
-                               WordCounter.new, Order::DEPTH_FIRST ),
-           ExtractFilter.new( [ :source_tree.to_k ] ) ]
-    chain = filter_chain( fc, :fragment )
-    assert( chain.filter( map ) )
-    assert_equal( "A more substantive paragraph.", map.extract.to_s )
+    htests = [ [ '<div></div>',          nil ],
+               [ '<div>too short</div>', nil ],
+               [ <<-'HTML', nil ],
+                 <div>
+                   <p>too short</p>
+                 </div>
+                 HTML
+               [ <<-'HTML', nil ],
+                 <div>one two<br/>
+                      three four</div>
+                 HTML
+               [ <<-'HTML', "one two three four" ],
+                 <div>one two three four</div>
+                 HTML
+               [ <<-'HTML', "one two three four" ],
+                 <div>
+                   <p>one two three four</p>
+                 </div>
+                 HTML
+               [ <<-'HTML', "one two three four" ],
+                 <div>one two three four<br/>
+                      not part of extract</div>
+                 HTML
+               [ <<-'HTML', "one two three four" ],
+                 <div>not<br/>
+                      one two three four
+                      </div>
+                 HTML
+               [ <<-'HTML', "A more substantive paragraph." ],
+                 <div>
+                   <p>Short junk</p>
+                   <hr/>
+                   <p>A more <i>substantive </i>paragraph.</p>
+                   <p>A similarly <i>substantive </i>paragraph.</p>
+                 </div>
+                 HTML
+               [ <<-'HTML', "a1 a2 a3 a4 a5 a6 a7 a8" ],
+                 <div>
+                   <p>a1 a2 a3</p>
+                   <p>a1 a2 a3 a4 a5</p>
+                   <p>a1 a2 a3 a4 a5 a6</p>
+                   <p>a1 a2 a3 a4 a5 a6 a7</p>
+                   <p>a1 a2 a3 a4 a5 a6 a7 a8</p>
+                   <p>a1 a2 a3 a4 a5 a6 a7 a8 a9</p>
+                 </div>
+                 HTML
+               [ <<-'HTML', "a1 a2 a3 a4 a5 a6 a7 a8" ],
+                 <div>
+                   <p>a1 a2 a3 a4 a5 a6 a7</p>
+                   <p>a1 a2 a3</p>
+                   <p>a1 a2 a3 a4 a5</p>
+                   <p>a1 a2 a3 a4 a5 a6</p>
+                   <p>a1 a2 a3 a4 a5 a6 a7 a8</p>
+                   <p>a1 a2 a3 a4 a5 a6 a7 a8 a9</p>
+                 </div>
+                 HTML
+             ]
+
+    htests.each do |html,exp_extract|
+      map = content( html )
+      tfc = TreeFilterChain.new( [ CharactersNormalizer.new,
+                                   WordCounter.new ] )
+      fc = [ HTMLTreeFilter.new( :source_tree.to_k, tfc, Order::DEPTH_FIRST ),
+             ExtractFilter.new( [ :source_tree.to_k ] ) ]
+      chain = filter_chain( fc, :fragment )
+      assert( chain.filter( map ) )
+      assert_equal( exp_extract, map.extract && map.extract.to_s,
+                    "from:\n" + html )
+    end
   end
 
 end
