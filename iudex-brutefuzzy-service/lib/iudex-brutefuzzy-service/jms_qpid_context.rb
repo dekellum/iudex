2e866644d26dd0f2f9b6847fc52c3a301eb4541b
David Kellum <dek-oss@gravitext.com>
2011-01-15 20:16:36 -0800

    brutefuzzy-service: Tested context and gem trappings

--- iudex-brutefuzzy-service/lib/iudex-brutefuzzy-service/jms_qpid_context.rb
+++ iudex-brutefuzzy-service/lib/iudex-brutefuzzy-service/jms_qpid_context.rb
@@ -1,25 +1,43 @@
+#--
+# Copyright (c) 2011 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+require 'rjack-qpid-client'
+
 module Iudex
 
   class JMSQpidContext
-    include iudex.jms.JMSContext
+    include Java::iudex.jms.JMSContext
 
     import 'java.util.Properties'
     import 'javax.naming.Context'
     import 'javax.naming.InitialContext'
     import 'javax.jms.Session';
 
-    attr_accessor :username
-    attr_accessor :password
+    attr_accessor :username  #Required non-nil
+    attr_accessor :password  #Required non-nil
     attr_accessor :brokers
     attr_accessor :virtual_host
     attr_accessor :client_id
     attr_accessor :factory_jndi_name
     attr_accessor :session_acknowledge_mode
-    attr_accesoor :destinations
+    attr_accessor :destinations
 
     def initialize
-      @username = nil
-      @password = nil
+      @username = 'qpid'
+      @password = 'password'
       @brokers = [ [ "localhost" ] ]
 
       @virtual_host = 'default-vhost'
@@ -77,7 +95,7 @@ module Iudex
     end
 
     # Serialize destination addresses (new format)
-    # http://qpid.apache.org/books/0.8/Programming-In-Apache-Qpid/html/ch02s04.html 
+    # http://qpid.apache.org/books/0.8/Programming-In-Apache-Qpid/html/ch02s04.html
     # Reference: section 2.3.4.5
     def address_serialize( address, subject = nil, opts = nil )
       out = address.to_s
