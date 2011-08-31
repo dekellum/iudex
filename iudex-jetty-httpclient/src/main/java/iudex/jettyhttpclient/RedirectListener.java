c1c9b346d7026ea698b1c0c2dee1d6e07b6ad9aa
David Kellum <dek-oss@gravitext.com>
2011-08-31 16:29:35 -0700

    jetty-httpclient: Import full RedirectListener from jetty 7.4.5 and fix Host header.

--- iudex-jetty-httpclient/src/main/java/iudex/jettyhttpclient/RedirectListener.java
+++ iudex-jetty-httpclient/src/main/java/iudex/jettyhttpclient/RedirectListener.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011 David Kellum
+ * Copyright (c) 2008-2009 Mort Bay Consulting Pty. Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License.  You may
@@ -16,19 +17,169 @@
 
 package iudex.jettyhttpclient;
 
+import java.io.IOException;
+
+import org.eclipse.jetty.client.Address;
 import org.eclipse.jetty.client.HttpDestination;
+import org.eclipse.jetty.client.HttpEventListener;
+import org.eclipse.jetty.client.HttpEventListenerWrapper;
 import org.eclipse.jetty.client.HttpExchange;
+import org.eclipse.jetty.http.HttpHeaders;
+import org.eclipse.jetty.http.HttpSchemes;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.io.Buffer;
 
 /**
  * Attempt to fix RedirectListener
  */
-public class RedirectListener extends org.eclipse.jetty.client.RedirectListener
+public class RedirectListener extends HttpEventListenerWrapper
 {
-    public RedirectListener( HttpDestination destination, HttpExchange ex )
+    private final HttpExchange _exchange;
+    private HttpDestination _destination;
+    private String _location;
+    private int _attempts;
+    private boolean _requestComplete;
+    private boolean _responseComplete;
+    private boolean _redirected;
+
+    public RedirectListener(HttpDestination destination, HttpExchange ex)
     {
-        super( destination, ex );
+        // Start of sending events through to the wrapped listener
+        // Next decision point is the onResponseStatus
+        super(ex.getEventListener(),true);
+
+        _destination = destination;
+        _exchange = ex;
     }
 
+    @Override
+    public void onResponseStatus( Buffer version, int status, Buffer reason )
+        throws IOException
+    {
+        _redirected = ((status == HttpStatus.MOVED_PERMANENTLY_301 ||
+                        status == HttpStatus.MOVED_TEMPORARILY_302) &&
+                       _attempts < _destination.getHttpClient().maxRedirects());
+
+        if (_redirected)
+        {
+            setDelegatingRequests(false);
+            setDelegatingResponses(false);
+        }
+
+        super.onResponseStatus(version,status,reason);
+    }
+
+    @Override
+    public void onResponseHeader( Buffer name, Buffer value )
+        throws IOException
+    {
+        if (_redirected)
+        {
+            int header = HttpHeaders.CACHE.getOrdinal(name);
+            switch (header)
+            {
+                case HttpHeaders.LOCATION_ORDINAL:
+                    _location = value.toString();
+                    break;
+            }
+        }
+        super.onResponseHeader(name,value);
+    }
+
+    @Override
+    public void onRequestComplete() throws IOException
+    {
+        _requestComplete = true;
+
+        if (checkExchangeComplete())
+        {
+            super.onRequestComplete();
+        }
+    }
+
+    @Override
+    public void onResponseComplete() throws IOException
+    {
+        _responseComplete = true;
+
+        if (checkExchangeComplete())
+        {
+            super.onResponseComplete();
+        }
+    }
+
+    public boolean checkExchangeComplete()
+        throws IOException
+    {
+        if (_redirected && _requestComplete && _responseComplete)
+        {
+            if (_location != null)
+            {
+                if (_location.indexOf("://")>0)
+                    _exchange.setURL(_location);
+                else
+                    _exchange.setURI(_location);
+
+                // destination may have changed
+                boolean isHttps = HttpSchemes.HTTPS.equals(String.valueOf(_exchange.getScheme()));
+                HttpDestination destination=_destination.getHttpClient().getDestination(_exchange.getAddress(),isHttps);
+
+                if (_destination==destination)
+                    _destination.resend(_exchange);
+                else
+                {
+                    // unwrap to find ultimate listener.
+                    HttpEventListener listener=this;
+                    while(listener instanceof HttpEventListenerWrapper)
+                        listener=((HttpEventListenerWrapper)listener).getEventListener();
+                    //reset the listener
+                    _exchange.getEventListener().onRetry();
+                    _exchange.reset();
+                    _exchange.setEventListener(listener);
+
+                    // DEK: Fixup the Host header
+                    Address adr = _exchange.getAddress();
+                    int port = adr.getPort();
+                    StringBuilder hh = new StringBuilder( 64 );
+                    hh.append( adr.getHost() );
+                    if( !( ( port == 80 && !isHttps ) ||
+                           ( port == 443 && isHttps ) ) ) {
+                        hh.append( ':' );
+                        hh.append( port );
+                    }
+                    _exchange.setRequestHeader( HttpHeaders.HOST, hh.toString() );
+
+                    destination.send(_exchange);
+                }
+
+                return false;
+            }
+            else
+            {
+                setDelegationResult(false);
+            }
+        }
+
+        return true;
+    }
+
+    public void onRetry()
+    {
+        _redirected=false;
+        _attempts++;
+
+        setDelegatingRequests(true);
+        setDelegatingResponses(true);
+
+        _requestComplete=false;
+        _responseComplete=false;
+
+        super.onRetry();
+    }
+
+    /**
+     * DEK: Delegate failed connection
+     */
     @Override
     public void onConnectionFailed( Throwable ex )
     {
@@ -38,6 +189,9 @@ public class RedirectListener extends org.eclipse.jetty.client.RedirectListener
         super.onConnectionFailed( ex );
     }
 
+    /**
+     * DEK: Delegate onException
+     */
     @Override
     public void onException( Throwable ex )
     {
