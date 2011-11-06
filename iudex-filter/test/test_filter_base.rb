b3b52b4064602683d04c772f67344a5597ae60ed
David Kellum <dek-oss@gravitext.com>
2011-11-05 18:11:47 -0700

    filter: Add test for round-trip of FilterException from ruby

--- iudex-filter/test/test_filter_base.rb
+++ iudex-filter/test/test_filter_base.rb
@@ -29,6 +29,8 @@ class TestFilterBase < MiniTest::Unit::TestCase
   include Iudex::Filter
   include Gravitext::HTMap
 
+  import 'iudex.filter.FilterListener'
+
   def test_base_name
     f = FilterBase.new
     assert_equal( "i.f.FilterBase", f.name )
@@ -37,6 +39,34 @@ class TestFilterBase < MiniTest::Unit::TestCase
   def test_top_level_name
     f = TestFilter.new
     assert_equal( "TestFilter", f.name )
+  end
+
+  # Filter exception may be raised from Ruby, and is handled as per
+  # Java: caught by chain, forwarded to listener as failure
+  def test_raise_filter_exception
+
+    test_filter = FilterBase.new
+    def test_filter.filter( map )
+      raise FilterException.new( "Expected" )
+    end
+
+    listener = FilterListener.new
+    class << listener
+      attr_accessor :fail
+      def failed( filter, map, x )
+        @fail = [ filter, map, x ]
+      end
+    end
+
+    fc = Core::FilterChain.new( "test", [ test_filter ] )
+    fc.listener = listener
+
+    map = UniMap.new
+    refute( fc.filter( map ) )
+
+    assert_equal( test_filter, listener.fail[0] )
+    assert_equal( map,         listener.fail[1] )
+    assert_instance_of( FilterException, listener.fail[2] )
   end
 
 end
