336e9ace75e0182b363b2226ea8f7a419057380d
David Kellum <dek-oss@gravitext.com>
2012-01-04 00:38:31 -0800

    brutefuzzy-protobuf: logback not needed in test/setup

--- iudex-brutefuzzy-protobuf/test/setup.rb
+++ iudex-brutefuzzy-protobuf/test/setup.rb
@@ -21,18 +21,3 @@ require 'bundler/setup'
 
 require 'minitest/unit'
 require 'minitest/autorun'
-
-require 'rjack-logback'
-
-module TestSetup
-  include RJack
-  Logback.config_console( :stderr => true, :thread => true )
-
-  if ( ARGV & %w[ -v --verbose --debug ] ).empty?
-    Logback.root.level = Logback::INFO
-  else
-    Logback.root.level = Logback::DEBUG
-  end
-
-  ARGV.delete( '--debug' )
-end
