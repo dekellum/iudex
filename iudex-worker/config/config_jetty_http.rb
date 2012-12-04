da9afa73d19e21c2e8f1c19d4dc685592cdaa737
David Kellum <dek-oss@gravitext.com>
2012-12-04 11:20:38 -0800

    worker: Update config_jetty_http.rb sample

--- iudex-worker/config/config_jetty_http.rb
+++ iudex-worker/config/config_jetty_http.rb
@@ -3,9 +3,11 @@ require 'iudex-jetty-httpclient'
 Iudex.configure do |c|
 
   c.setup_jetty_httpclient do
-    { :timeout => 20_000,
+    { :connect_timeout => 12_000,
+      :idle_timeout    => 20_000,
       :max_connections_per_address => 2,
-      :max_queue_size_per_address => 20 }
+      :max_queue_size_per_address => 20,
+      :follow_redirects => false }
   end
 
 end
