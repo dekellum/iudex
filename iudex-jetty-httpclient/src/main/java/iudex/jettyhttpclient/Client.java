09e062b3c12a46e29ab0e3cc5a8ad8ee52b82152
David Kellum <dek-oss@gravitext.com>
2012-12-27 16:45:16 -0800

    jetty: M4 getURI returns URI

--- iudex-jetty-httpclient/src/main/java/iudex/jettyhttpclient/Client.java
+++ iudex-jetty-httpclient/src/main/java/iudex/jettyhttpclient/Client.java
@@ -282,7 +282,7 @@ public class Client
                 _requestHeaders.add(new Header(f.getName(), f.getValue()));
             }
 
-            setUrl( request.getURI() );
+            setUrl( request.getURI().toString() );
         }
 
         @Override
