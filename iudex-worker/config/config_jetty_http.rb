e7aba30c37dcee0ac838ac779886b651281a9ce6
David Kellum <dek-oss@gravitext.com>
2013-01-09 11:32:32 -0800

    worker: Update config jetty sample for M4 per_destination options

--- iudex-worker/config/config_jetty_http.rb
+++ iudex-worker/config/config_jetty_http.rb
@@ -5,8 +5,8 @@ Iudex.configure do |c|
   c.setup_jetty_httpclient do
     { :connect_timeout => 12_000,
       :idle_timeout    => 20_000,
-      :max_connections_per_address => 2,
-      :max_queue_size_per_address => 20,
+      :max_connections_per_destination => 2,
+      :max_requests_queued_per_destination => 20,
       :follow_redirects => false }
   end
 
