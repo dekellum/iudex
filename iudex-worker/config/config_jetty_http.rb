c5f087f1964b16b9e070c50ba0cc0bdcbe7cee66
David Kellum <dek-oss@gravitext.com>
2013-06-25 17:58:14 -0700

    worker: Add top level :timeout back to config_jetty_http.rb sample

--- iudex-worker/config/config_jetty_http.rb
+++ iudex-worker/config/config_jetty_http.rb
@@ -3,7 +3,8 @@ require 'iudex-jetty-httpclient'
 Iudex.configure do |c|
 
   c.setup_jetty_httpclient do
-    { :connect_timeout => 12_000,
+    { :timeout         => 35_000,
+      :connect_timeout => 12_000,
       :idle_timeout    => 20_000,
       :max_connections_per_destination => 2,
       :max_requests_queued_per_destination => 20,
